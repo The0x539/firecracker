a806a278f3a0295f232a573e334e57bb4fff4554
Andrew Neth <the0x539@gmail.com>
2019-09-03 14:32:22 -0500

    when the guest performs port I/O on the magic HVM port, just print the data

--- vmm/src/vstate.rs
+++ vmm/src/vstate.rs
@@ -350,8 +350,8 @@ impl Vcpu {
     fn run_emulation(&mut self) -> Result<()> {
         match self.fd.run() {
             Ok(run) => match run {
-                VcpuExit::IoOut(0x7C4, _data) => {
-                    println!("output on magic port");
+                VcpuExit::IoOut(0x7C4, data) => {
+                    println!("output on magic port: {:#02X} {:02X} {:02X} {:02X}", data[3], data[2], data[3], data[0]);
                     Ok(())
                 }
                 VcpuExit::IoIn(addr, data) => {
