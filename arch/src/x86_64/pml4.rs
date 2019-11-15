deeff13e7f8ca8572c00abd60355dc4e1648e0ef
Andrew Neth <the0x539@gmail.com>
2019-11-15 15:39:23 -0600

    PML4: legacy non-PAE page table data structures

--- arch/src/x86_64/pml4.rs
+++ arch/src/x86_64/pml4.rs
@@ -80,6 +80,53 @@ bitfield! {
     pub no_execute, set_no_execute: 63;
 }
 
+bitfield! {
+    pub struct LegacyPDe(u32);
+    pub present, set_present: 0;
+    pub writable, set_writable: 1;
+    pub user_page, set_user_page: 2;
+    pub write_through, set_write_through: 3;
+    pub cache_disable, set_cache_disable: 4;
+    pub accessed, set_accessed: 5;
+    // IGN, _: 6;
+    pub is_4mb, set_4mb: 7;
+    // IGN, _: 7;
+    pub vmm_info, set_vmm_info: 11, 9;
+    pub pt_base_addr, set_pt_base_addr: 31, 12;
+
+    // 4MB fields
+    pub dirty, set_dirty: 6;
+    pub global, set_global: 8;
+    pub pat, set_pat: 12;
+    pub page_base_addr_lo, set_page_base_addr_lo: 31, 22;
+    pub page_base_addr_hi, set_page_base_addr_hi: 20, 13;
+
+}
+impl LegacyPDe {
+    fn page_base_addr(&self) -> u33 {
+        (self.page_base_addr_hi() << 10) | (self.page_base_addr_lo())
+    }
+    fn set_page_base_addr(&mut self, val: u32) {
+        self.set_page_base_addr_lo(val & 0x3ff /* lower 10 bits */);
+        self.set_page_base_addr_hi(val >> 10);
+    }
+}
+
+bitfield! {
+    pub struct LegacyPTe(u32);
+    pub present, set_present: 0;
+    pub writable, set_writable: 1;
+    pub user_page, set_user_page: 2;
+    pub write_through, set_write_through: 3;
+    pub cache_disable, set_cache_disable: 4;
+    pub accessed, set_accessed: 5;
+    pub dirty, set_dirty: 6;
+    pub pat, set_pat: 7;
+    pub global, set_global: 8;
+    pub vmm_info, set_vmm_info: 11, 9;
+    pub page_base_addr, set_page_base_addr: 31, 12;
+}
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum PagingLevel {
     Normal,
